fd6b852b28751aa4e9c094b97316b8d8633bbe26
shinya yoshitaka <ushimiyay20@gmail.com>
2018-10-27 14:32:19 +0900

    add_source

--- src/django/JphacksProject/JphacksProject/urls.py
+++ src/django/JphacksProject/JphacksProject/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api_clover/', include(('api_clover.urls', 'api_clover'), )),
 ]
